ef350e60a59780bd81931879e6ce94cab9833c59
Markus <realim1@unlv.nevada.edu>
2018-06-25 20:27:00 -0700

    Enhanced search to filter pokemon by type. Developed some functions for creating the pokemon's info page.
    Enhanced search to filter pokemon by type. Developed some functions for creating the pokemon's info page.

--- pokedex/lib/main.dart
+++ pokedex/lib/main.dart
@@ -21,9 +21,11 @@ class Pokemon{
   final String name;
   final String idNum;
   final String url;
+  final String height;
+  final String weight;
   final List type;
   final List weaknesses;
-  final double spawnChance;
+  //final double spawnChance;
 
 
   Pokemon.fromJsonMap(Map map)
@@ -31,12 +33,94 @@ class Pokemon{
     idNum = map['num'],
     url = map['img'],
     type = map['type'],
-    weaknesses = map['weaknesses'],
-    spawnChance = map['spawn_chance'];
+    height = map['height'],
+    weight = map['weight'],
+    weaknesses = map['weaknesses'];
+    //spawnChance = map['spawn_chance'];
 
 }
 
 class PokemonInfoPage extends StatelessWidget{
+  
+
+  Color getColor(String type){
+    if(type == 'Fire')
+      return Color(0xFFED813A);
+    else if(type == 'Grass')
+      return Color(0xFF7BC658);
+    else if(type == 'Water')
+      return Color(0xFF6B91EA);
+    else if(type == 'Fighting')
+      return Color(0xFFC12E30);
+    else if(type == 'Flying')
+      return Color(0xFFAA91EE);
+    else if(type == 'Normal')
+      return Color(0xFFA9A678);
+    else if(type == 'Poison')
+      return Color(0xFF9E45A2);
+    else if(type == 'Electric')
+      return Color(0xFFF6CB3E);
+    else if(type == 'Ground')
+      return Color(0xFFDFBA67);
+    else if(type == 'Psychic')
+      return Color(0xFFFB5B8B);
+    else if(type == 'Rock')
+      return Color(0xFFB5A040);
+    else if(type == 'Ice')
+      return Color(0xFF9ED6D3);
+    else if(type == 'Bug')
+      return Color(0xFFA7B440);
+    else if(type == 'Dragon')
+      return Color(0xFF7143F4);
+    else if(type == 'Ghost')
+      return Color(0xFF6E599C);
+    else if(type == 'Dark')
+      return Color(0xFF6E584A);
+    else if(type == 'Steel')
+      return Color(0xFFB7B9D1);
+    else
+      return Color(0xFFEA9BAE);
+  }
+
+  Image getImage(String type){
+      if(type == 'Fire')
+      return Image.network('http://www.top4themes.com/data/out/49/5744584-fire-pokemon-wallpapers.png');
+    else if(type == 'Grass')
+      return Image.network('https://img00.deviantart.net/f80b/i/2014/042/7/d/grass_pokemon_energy_wallpaper_by_elbarnzo-d755hbj.png');
+    else if(type == 'Water')
+      return Image.network('https://img00.deviantart.net/8a84/i/2014/042/8/6/water_pokemon_energy_wallpaper_by_elbarnzo-d75ebft.png');
+    else if(type == 'Fighting')
+      return Image.network('https://img00.deviantart.net/9d02/i/2014/042/0/9/fighting_pokemon_energy_wallpaper_by_elbarnzo-d74vo4c.png');
+    else if(type == 'Flying')
+      return Image.network('https://pre00.deviantart.net/b623/th/pre/f/2016/317/f/7/flying_type_pokemon_energy_by_elbarnzo-daoas8t.png');
+    else if(type == 'Normal')
+      return Image.network('https://img00.deviantart.net/cb67/i/2014/031/0/b/colorless_pokemon_energy_wallpaper_by_elbarnzo-d74i220.png');
+    else if(type == 'Poison')
+      return Image.network('https://pre00.deviantart.net/7ce7/th/pre/f/2017/209/f/b/poison_type_pokemon_wallpaper_by_elbarnzo-dbi2b02.png');
+    else if(type == 'Electric')
+      return Image.network('https://t00.deviantart.net/yBj5j6qmTCOUIvWofWLf97Pii9Y=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/23b1/th/pre/i/2014/042/3/2/electric_pokemon_energy_wallpaper_by_elbarnzo-d74n7g8.png');
+    else if(type == 'Ground')
+      return Image.network('https://pre00.deviantart.net/5049/th/pre/f/2017/209/d/c/ground_type_pokemon_wallpaper_by_elbarnzo-dbi2ame.png');
+    else if(type == 'Psychic')
+      return Image.network('https://img00.deviantart.net/3d74/i/2014/042/b/5/physic_pokemon_energy_wallpaper_by_elbarnzo-d759r4f.png');
+    else if(type == 'Rock')
+      return Image.network('https://pre00.deviantart.net/5117/th/pre/f/2017/209/9/6/rock_type_pokemon_wallpaper_by_elbarnzo-dbi2b77.png');
+    else if(type == 'Ice')
+      return Image.network('https://pre00.deviantart.net/a5f3/th/pre/f/2017/209/0/f/ice_type_pokemon_wallpaper_by_elbarnzo-dbi2axg.png');
+    else if(type == 'Bug')
+      return Image.network('https://pre00.deviantart.net/848d/th/pre/f/2016/275/6/7/bug_type_pokemon_wallpaper_by_elbarnzo-dajncbl.png');
+    else if(type == 'Dragon')
+      return Image.network('https://pre00.deviantart.net/206b/th/pre/f/2016/279/4/b/dragon_type_pokemon_wallpaper_by_elbarnzo-dak4w59.png');
+    else if(type == 'Ghost')
+      return Image.network('https://img00.deviantart.net/c868/i/2016/326/b/8/ghost_type_pokemon_wallpaper_by_elbarnzo-dapamlc.png');
+    else if(type == 'Dark')
+      return Image.network('https://pre00.deviantart.net/9c3b/th/pre/f/2016/277/b/2/dark_type_pokemon_wallpaper_by_elbarnzo-dajx3sf.png');
+    else if(type == 'Steel')
+      return Image.network('https://pre00.deviantart.net/cb69/th/pre/f/2017/209/a/9/steel_type_pokemon_wallpaper_by_elbarnzo-dbi2bbj.png');
+    else
+      return Image.network('https://pre00.deviantart.net/5aaa/th/pre/f/2016/317/d/b/fairy_type_pokemon_wallpaper_by_elbarnzo-daoarj9.png');
+  }
+  
   @override
   PokemonInfoPage(this.pokemon);
   final Pokemon pokemon;
@@ -44,10 +128,61 @@ class PokemonInfoPage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
+        backgroundColor: getColor(pokemon.type[0]),
         title:Text(pokemon.name)
         ),
+      body: ListView(
+        children: <Widget>[
+
+          Stack(children: <Widget>[
+
+            Container(
+              child: getImage(pokemon.type[0])
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 150.0),
+              child: Center(
+                child: Image.network(pokemon.url),  
+              )
+            ),
+    
+          ],),
+          Container(
+            child: pokemon.type.length > 1 
+            ? Row(
+              children: <Widget>[
+                Container(
+                  width: 200.0,
+                  child:Chip(
+                    backgroundColor: getColor(pokemon.type[0]),
+                    label: Center(child:Text(pokemon.type[0])),
+                  )
+                ),
+                Container(
+                  width: 200.0,
+                  child:Chip(
+                    backgroundColor: getColor(pokemon.type[1]),
+                    label: Center(child:Text(pokemon.type[1])),
+                  )
+                )
+              ],
+            )
+          : Center(
+            child: Container(
+              width: 200.0,
+              child:Chip(
+                backgroundColor: getColor(pokemon.type[0]),
+                label: Center(child:Text(pokemon.type[0]))
+              )
+            )
+          )
+            
+          )
+        ],
+
+      )
     );
+    
   }
 
 
@@ -70,8 +205,15 @@ onSearchTextChanged(String text) async{
   }
   
   list.forEach((pokemonItem){
-    if(pokemonItem.name.contains(text) || pokemonItem.idNum.contains(text))
+    text = text[0].toUpperCase() + text.substring(1);
+    if(pokemonItem.type.length == 1){
+    if(pokemonItem.name.contains(text) || pokemonItem.idNum.contains(text) || pokemonItem.type[0].contains(text))
       searchResults.add(pokemonItem);
+    }
+    else{
+      if(pokemonItem.name.contains(text) || pokemonItem.idNum.contains(text) || pokemonItem.type[0].contains(text) || pokemonItem.type[1].contains(text))
+      searchResults.add(pokemonItem);
+    }
   });
 
   setState(() {
@@ -105,7 +247,6 @@ onSearchTextChanged(String text) async{
     streamController = null;
   }
 
-  //Calls our getData function on initial start of application
   @override
   void initState(){
     super.initState();
